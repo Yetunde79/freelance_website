6de474c3d3e23fa80baae03216f6cf8892549d9e
Yetunde Sola-Adebayo <ysolaadebayo@gmail.com>
2019-05-23 18:26:51 -0500

    Updates

---
+++ precache-manifest.34f1e8a63aa22c37d3b49fa691f82a91.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce657e8c7601304916687592c1faa3b2",
+    "url": "/static/media/fylo.ce657e8c.png"
+  },
+  {
+    "revision": "4f409fe0f2ae48029345",
+    "url": "/static/css/main.9ebd71fd.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4a8ee30f6ea144bde62b",
+    "url": "/static/js/2.6c735f15.chunk.js"
+  },
+  {
+    "revision": "fa6d5dc55d29be67447a1940c5601b22",
+    "url": "/static/media/aimient.fa6d5dc5.png"
+  },
+  {
+    "revision": "c1aa8144add8fe03b96da76fadc9b3aa",
+    "url": "/static/media/jac9ja.c1aa8144.png"
+  },
+  {
+    "revision": "b655fe98e89f4d21e0083035fa02d657",
+    "url": "/static/media/shoot.b655fe98.png"
+  },
+  {
+    "revision": "473fbaa57068ae5f50af85d76306c335",
+    "url": "/static/media/yettifood.473fbaa5.png"
+  },
+  {
+    "revision": "0828ed6a85329f67637d719cacd21a84",
+    "url": "/static/media/shopify.0828ed6a.png"
+  },
+  {
+    "revision": "4f409fe0f2ae48029345",
+    "url": "/static/js/main.7fdfbb79.chunk.js"
+  },
+  {
+    "revision": "446eaab8033e906b4b040bfcbfb2e7cd",
+    "url": "/static/media/cool-background.446eaab8.png"
+  },
+  {
+    "revision": "bfeecf2598f0d687e6b20473ac589637",
+    "url": "/static/media/quote.bfeecf25.svg"
+  },
+  {
+    "revision": "75eccbfe8474bdade88bcb148cb54921",
+    "url": "/static/media/mobile-devices.75eccbfe.svg"
+  },
+  {
+    "revision": "16f46238f4404a47adcee81581c36a7a",
+    "url": "/static/media/seo.16f46238.svg"
+  },
+  {
+    "revision": "02fdd1c00d23346b001d4596ce354129",
+    "url": "/static/media/wordpress.02fdd1c0.svg"
+  },
+  {
+    "revision": "4e9bc0cf9c49e9d17e325149310496eb",
+    "url": "/static/media/shopify.4e9bc0cf.svg"
+  },
+  {
+    "revision": "75221c0621b45d7ae54b206d521502e2",
+    "url": "/static/media/code-solid.75221c06.svg"
+  },
+  {
+    "revision": "e2f2271b19f37dbcbe52ff9039d83cb4",
+    "url": "/static/media/party-popper.e2f2271b.svg"
+  },
+  {
+    "revision": "3c126b0ad18756df74c45f4d809e66aa",
+    "url": "/index.html"
+  }
+];
